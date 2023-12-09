17b1e2eea476821a5aaa2e30d87d06b803e4f0d3
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-09 15:53:32 +0530

    handling root path

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,14 @@ app.use(cors())
 app.use("/users", userRoutes);
 app.use('/tasks', taskRoutes);
 
+app.use('/', (req, res) => {
+  res.send(`
+    <h1>Home</h1>
+    <a href="/users"><button>users</button></a>
+    <a href="/tasks"><button>tasks</button></a>
+  `)
+});
+
 app.listen(PORT, () => {
   console.log(`Server Is Running At PORT ${PORT}`);
 });
